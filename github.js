83f1c0e8cc4e52812f75ad81c85739235dfd3d8a
Devesh <devsrawt@gmail.com>
2018-09-24 22:15:16 +0530

    GitHub Finder App

---
+++ github.js
@@ -0,0 +1,20 @@
+class Github {
+    constructor() {
+        this.client_id = '4e69e93a03712957ef28';
+        this.client_secret = '63fb4db36be74a6dae2ec564c3b861946d0f2c39';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
